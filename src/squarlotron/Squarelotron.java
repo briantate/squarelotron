c4a8e1fad572453caafa2f2817e95bc030d4872b
btate <brian.c.tate@gmail.com>
2021-06-17 05:20:12 -0500

    add diagonal flip and rotation methods

--- src/squarlotron/Squarelotron.java
+++ src/squarlotron/Squarelotron.java
@@ -6,10 +6,14 @@ public class Squarelotron {
     int size;
     int[][] squarelotron;
 
+    // private data
+    private int numRings;
+
     /* constructor */
     Squarelotron(int size) {
 	this.size = size;
 	squarelotron = new int[size][size];
+	this.numRings = (this.size / 2) + (this.size % 2);
 
 	for (int i = 0; i < this.size; i++) {
 	    for (int j = 0; j < this.size; j++) {
@@ -29,63 +33,118 @@ public class Squarelotron {
 	    System.out.print("\r\n");
 	}
     }
-    
+
     public Squarelotron upsideDownFlip(int ring) {
-	//stub
-	if(1 > ring)
-	{
+
+	if ((1 > ring ) || (ring > this.numRings)) {
 	    System.out.print("invalid ring\r\n");
 	}
-	
-	//create a new squarelotron and modify it
+
+	// create a new squarelotron and modify it
 	Squarelotron anotherSquarelotron = new Squarelotron(this.size);
-	//copy data into new squarelotron
+	// copy data into new squarelotron
 	for (int i = 0; i < this.size; i++) {
 	    for (int j = 0; j < this.size; j++) {
 		anotherSquarelotron.squarelotron[i][j] = this.squarelotron[i][j];
 	    }
 	}
 	
-	//loop through all items and modify if needed
-	for (int i = 0; i < this.size; i++) {
-	    for (int j = 0; j < this.size; j++) {
-		//look for elements on the ring
-		if((i == ring - 1)||(i == this.size - ring) || (j == ring -1) || (j == size - ring)) {
-		    anotherSquarelotron.squarelotron[i][j] = this.squarelotron[Math.abs(i-(size-ring))][j];
+	// loop through all items and modify if needed
+	for (int i = ring - 1; i < this.size - ring + 1; i++) {
+	    for (int j = ring - 1; j < this.size - ring + 1; j++) {
+		// look for elements on the ring
+		if ((i == ring - 1) || (i == this.size - ring) || (j == ring - 1) || (j == size - ring)) {
+		    anotherSquarelotron.squarelotron[i][j] = this.squarelotron[size - i - 1][j];
 		}
 	    }
 	}
-	
+
 	return anotherSquarelotron;
-	
+
     }
-    
+
     public Squarelotron mainDiagonalFlip(int ring) {
-	//stub
+
+	if ((1 > ring ) || (ring > this.numRings)) {
+	    System.out.print("invalid ring\r\n");
+	}
+
+	// create a new squarelotron and modify it
 	Squarelotron anotherSquarelotron = new Squarelotron(this.size);
-	
+	// copy data into new squarelotron
+	for (int i = 0; i < this.size; i++) {
+	    for (int j = 0; j < this.size; j++) {
+		anotherSquarelotron.squarelotron[i][j] = this.squarelotron[i][j];
+	    }
+	}
+
+	// loop through all items and modify if needed
+	for (int i = ring - 1; i < this.size - ring + 1; i++) {
+	    for (int j = ring - 1; j < this.size - ring + 1; j++) {
+		// look for elements on the ring
+		if ((i == ring - 1) || (i == this.size - ring) || (j == ring - 1) || (j == size - ring)) {
+		    anotherSquarelotron.squarelotron[i][j] = this.squarelotron[j][i];
+		}
+	    }
+	}
+
 	return anotherSquarelotron;
-	
+
     }
-    
+
     public void rotateRight(int numberOfTurns) {
-	//stub
+
+	// temporary squarelotron to hold the initial state
+	int[][] tempSquare = new int[this.size][this.size];
+
+	// copy the initial contents to a temporary squarelotron
+	for (int i = 0; i < this.size; i++) {
+	    for (int j = 0; j < this.size; j++) {
+		tempSquare[i][j] = this.squarelotron[i][j];
+	    }
+	}
+
+	// flip numberOfTurns times
+	// ToDo:
+	// operate on all rings
+	for (int ring = 1; ring < this.numRings + 1; ring++) {
+	    System.out.printf("ring = %d\r\n",ring);   
+	    
+	    for(int moves = 0; moves < 3; moves++)//do this operation 3 times to rotate a ring
+	    {
+		System.out.println("moves =" + moves);
+		Squarelotron mainDiagSquarelotron;
+		mainDiagSquarelotron = this.mainDiagonalFlip(ring);
+		
+		Squarelotron upsideDownSquarelotron;
+		upsideDownSquarelotron = mainDiagSquarelotron.upsideDownFlip(ring);
+		
+		//copy new contents to original squarelotron
+		for (int i = 0; i < this.size; i++) {
+		    for (int j = 0; j < this.size; j++) {
+			this.squarelotron[i][j] = upsideDownSquarelotron.squarelotron[i][j];
+		    }
+		}
+		this.printSquarelotron();
+		System.out.print("\r\n\r\n");
+	    }
+	    this.printSquarelotron();
+	}
+
 	return;
     }
-    
-    
-    
+
     /* main method */
 
     public static void main(String[] args) {
 	Scanner scanner = new Scanner(System.in);
-	
+
 	System.out.println("Enter Squareletron size");
 	int size = scanner.nextInt();
-	
+
 	Squarelotron mySquarelotron = new Squarelotron(size);
 	mySquarelotron.printSquarelotron();
-	
+
 	/* clean up and end */
 	scanner.close();
 	return;
