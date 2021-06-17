e285f509c3d52e887f15bee23b188df2fc9ad89e
btate <brian.c.tate@gmail.com>
2021-06-17 05:20:46 -0500

    add junit tests for internal rings

--- src/squarlotron/SquarelotronTest.java
+++ src/squarlotron/SquarelotronTest.java
@@ -22,6 +22,9 @@ class SquarelotronTest {
 	
 	boolean failure = false;
 	
+	
+	//ring 1
+	
 	int[][] testSquarelotron = 
 	    {{21,22,23,24,25},
 	     {16, 7, 8, 9,20},
@@ -57,12 +60,44 @@ class SquarelotronTest {
 	newSquarelotron.printSquarelotron();
 	System.out.print("\r\n\r\n");
 	
-	if(failure) {
-	    System.out.println("fail");
-	    fail("wrong values");
-	}else {
-	    System.out.println("pass");
+	//ring 2
+	System.out.print("test2Squarelotron:\r\n");
+	int[][] test2Squarelotron = 
+	    {{ 1, 2, 3, 4, 5},
+	     { 6,17,18,19,10},
+	     {11,12,13,14,15},
+	     {16, 7, 8, 9,20},
+	     {21,22,23,24,25}};
+	
+	
+	for (int i = 0; i < size; i++) {
+	    for (int j = 0; j < size; j++) {
+		System.out.print(test2Squarelotron[i][j] + "\t");
+	    }
+	    System.out.print("\r\n");
 	}
+	
+	System.out.print("\r\n\r\n");
+	
+	Squarelotron ring2Squarelotron = mySquarelotron.upsideDownFlip(2);
+	
+	//compare
+	for (int i = 0; i < size; i++) {
+	    for (int j = 0; j < size; j++) {
+		if(test2Squarelotron[i][j] != ring2Squarelotron.squarelotron[i][j])
+		{
+		    failure = true;
+		    System.out.printf("err:[%d][%d]\r\n",i,j);
+		}
+	    }
+	}
+	
+	System.out.print("ring2Squarelotron:\r\n");
+	ring2Squarelotron.printSquarelotron();
+	System.out.print("\r\n\r\n");
+	
+	if(failure)
+	    fail("wrong values");
 	
 	
     }
@@ -74,7 +109,9 @@ class SquarelotronTest {
 	
 	boolean failure = false;
 	
-	int[][] testSquarelotron = 
+	//Ring 1 flip
+	
+	int[][] testSquarelotronRing1 = 
 	    {{ 1, 6,11,16,21},
 	     { 2, 7, 8, 9,22},
 	     { 3,12,13,14,23},
@@ -84,25 +121,65 @@ class SquarelotronTest {
 	System.out.print("testSquarelotron:\r\n");
 	for (int i = 0; i < size; i++) {
 	    for (int j = 0; j < size; j++) {
-		System.out.print(testSquarelotron[i][j] + "\t");
+		System.out.print(testSquarelotronRing1[i][j] + "\t");
 	    }
 	    System.out.print("\r\n");
 	}
 	
 	System.out.print("\r\n\r\n");
 	
-	Squarelotron newSquarelotron = mySquarelotron.mainDiagonalFlip(1);
+	Squarelotron ring1Squarelotron = mySquarelotron.mainDiagonalFlip(1);
 	
 	//compare
 	for (int i = 0; i < size; i++) {
 	    for (int j = 0; j < size; j++) {
-		if(testSquarelotron[i][j] != newSquarelotron.squarelotron[i][j])
+		if(testSquarelotronRing1[i][j] != ring1Squarelotron.squarelotron[i][j])
+		{
+		    System.out.printf("err:[%d][%d]\r\n",i,j);
 		    failure = true;
+		}
+		    
 	    }
 	}
 	
 	System.out.print("newSquarelotron:\r\n");
-	newSquarelotron.printSquarelotron();
+	ring1Squarelotron.printSquarelotron();
+	System.out.print("\r\n\r\n");
+	
+	//ring 2 flip
+	int[][] testSquarelotronRing2 = 
+	    {{ 1, 2, 3, 4, 5},
+	     { 6, 7,12,17,10},
+	     {11, 8,13,18,15},
+	     {16, 9,14,19,20},
+	     {21,22,23,24,25}};
+	
+	System.out.print("testSquarelotron2:\r\n");
+	for (int i = 0; i < size; i++) {
+	    for (int j = 0; j < size; j++) {
+		System.out.print(testSquarelotronRing2[i][j] + "\t");
+	    }
+	    System.out.print("\r\n");
+	}
+	
+	System.out.print("\r\n\r\n");
+	
+	Squarelotron ring2Squarelotron = mySquarelotron.mainDiagonalFlip(2);
+	
+	//compare
+	for (int i = 0; i < size; i++) {
+	    for (int j = 0; j < size; j++) {
+		if(testSquarelotronRing2[i][j] != ring2Squarelotron.squarelotron[i][j])
+		{
+		    System.out.printf("err:[%d][%d]\r\n",i,j);
+		    failure = true;
+		}
+		    
+	    }
+	}
+	
+	System.out.print("newSquarelotron:\r\n");
+	ring2Squarelotron.printSquarelotron();
 	System.out.print("\r\n\r\n");
 	
 	if(failure)
@@ -140,7 +217,10 @@ class SquarelotronTest {
 	for (int i = 0; i < size; i++) {
 	    for (int j = 0; j < size; j++) {
 		if(testSquarelotron[i][j] != mySquarelotron.squarelotron[i][j])
+		{
+		    System.out.printf("err:[%d][%d]\r\n",i,j);
 		    failure = true;
+		}
 	    }
 	}
 	
